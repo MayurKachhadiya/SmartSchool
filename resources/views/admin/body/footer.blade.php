7811e64113d6339671579c01bf03846228b1fb7e
MayurKachhadiya <mayurkachhadiya373@gmail.com>
2022-05-04 10:56:37 +0530

    footer rename

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -2,10 +2,10 @@
     <div class="pull-right d-none d-sm-inline-block">
         <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
 		  <li class="nav-item">
-			<a class="nav-link" href="javascript:void(0)">FAQ</a>
+			<a class="nav-link" href="javascript:void(0)">FAQs</a>
 		  </li>
 		 
 		</ul>
     </div>
-	  &copy; 2022 <a href="#">Group 61</a>. All Rights Reserved.
+	  &copy; 2022 <a href="#">Group 61</a>. All Rights Reserved....
   </footer>
